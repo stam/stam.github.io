75cefa7fb648cdc35914c278ccd45f5b515c8b7e
Jasper Stam <jasper@strnk.nl>
2019-11-27 20:30:05 +0100

    🚮 Cleanup logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as Three from "three";
 import FastSimplexNoise from "fast-simplex-noise";
-// import { OrbitControls } from "three-orbitcontrols-ts";
 
 const WIDTH = 50;
 const HEIGHT = 40;
@@ -20,7 +19,6 @@ export class Renderer {
   scene: Three.Scene;
   camera: Three.Camera;
 
-  _plane: Three.Geometry;
   _grid: Three.Mesh;
 
   constructor(canvas: HTMLCanvasElement) {
@@ -52,55 +50,31 @@ export class Renderer {
   }
 
   createGrid() {
-    const geometry = new Three.BoxGeometry(1, 1, 1);
-    const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new Three.Mesh(geometry, material);
-    // this.scene.add(cube);
-
     const plane = new Three.PlaneGeometry(200, 100, WIDTH, HEIGHT);
 
-    // const gridMaterial = new Three.MeshLambertMaterial();
     const gridMaterial = new Three.ShaderMaterial({
       vertexShader: document.getElementById("vertexShader").textContent,
       fragmentShader: document.getElementById("fragmentShader").textContent
     });
     this._grid = new Three.Mesh(plane, gridMaterial);
-    // this._grid.wire;
-
-    // this._grid = new Three.LineSegments(wireframeGeometry, lineMaterial);
 
     this.scene.add(this._grid);
   }
 
   setZ() {
-    // const grid = this._grid;
-    // grid.geometry
     const plane = this._grid.geometry as Three.Geometry;
-    // const p = this._plane;
     plane.verticesNeedUpdate = true;
-
-    console.log("l", plane.vertices.length);
 
     const noise = [];
     for (let x = 0; x < WIDTH; x++) {
-      // noise[x] = [];
       for (let y = 0; y < HEIGHT; y++) {
         const index = x * HEIGHT + y;
         const n = noiseGen.scaled([y + Y_OFFSET, x + X_OFFSET]);
-        // const n = noiseGen.scaled([x + X_OFFSET, y + Y_OFFSET]);
         noise[index] = `${n},${x},${y},${index}`;
-        const v = plane.vertices[index];
-        v.z = n;
+        plane.vertices[index].z = n;
       }
     }
     X_OFFSET += 0.1;
-    // console.log(noise);
-
-    // for (let i = 0; i < plane.vertices.length; i++) {
-    //   const v = plane.vertices[i];
-    //   v.z = Math.random();
-    //   // console.log(v.z);
-    // }
   }
 
   animate() {
