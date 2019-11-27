7782ba6bcc184adff4118b9d6f4dfbd8442f5f4f
Jasper Stam <jasper@strnk.nl>
2019-11-27 22:12:30 +0100

    🛩  Finish terrain flyover effect

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,10 @@ import { Perlin } from "libnoise-ts/module/generator";
 const WIDTH = 50;
 const HEIGHT = 40;
 
-let X_OFFSET = 0;
-let Y_OFFSET = 0;
+let X_OFFSET = 0.201;
+let Y_OFFSET = 0.101;
 
 const noiseGen = new Perlin();
-
-// const genNoise = (xOffset = 0) => {
-//   const g = [];
-//   for (let i = 0; i < 3; i++) {
-//     g[i] = Math.round(100 * noiseGen.noise2D(i + xOffset, 0)) / 100;
-//     // for (let j = 0; j < 3; j++) {
-//     //   g[i][j] = noiseGen.noise2D(i + xOffset, j);
-//     // }
-//   }
-//   console.log("noise", g);
-// };
 
 export class Renderer {
   renderer: Three.WebGLRenderer;
@@ -65,58 +54,27 @@ export class Renderer {
     this._grid = new Three.Mesh(plane, gridMaterial);
 
     this.scene.add(this._grid);
-
-    //@ts-ignore
-    window.p = plane;
-
-    // plane.vertices.forEach((v, i) => {
-    //   if (i <= 51) {
-    //     console.log(v);
-    //   }
-    // });
-
-    // const reducer = (acc: Vector3, currentValue: Vector3) => {
-    //   console.log("currentValue", currentValue);
-    //   return 0;
-    // };
-    // const a = plane.vertices.reduce(reducer);
   }
 
   setZ() {
     const plane = this._grid.geometry as Three.Geometry;
     plane.verticesNeedUpdate = true;
 
-    for (let x = 0; x < HEIGHT; x++) {
-      for (let y = 0; y < WIDTH; y++) {
-        const index = y + x * (WIDTH + 1);
+    for (let row = 0; row < HEIGHT; row++) {
+      for (let col = 0; col < WIDTH; col++) {
+        const index = col + row * (WIDTH + 1);
 
-        // if (x === 0 && [0, 1, 3, 4].includes(y)) {
-        //   console.log(plane.vertices[index]);
-        // }
-        // console.log(x, y, index, plane.vertices[index]);
-        // }
-        // if (x === 1) {
-        //   console.log(x, y, plane.vertices[index]);
-        // }
-
-        // console.log("xoff", X_OFFSET);
+        const { x, y } = plane.vertices[index];
         const noiseValue =
-          X_OFFSET % WIDTH === x || X_OFFSET % HEIGHT === HEIGHT - y ? 2 : 0;
-        // const noiseValue = noiseGen.getValue(x, 10, 0) * 4;
+          noiseGen.getValue(X_OFFSET + x / 20, Y_OFFSET + y / 20, 0) * 4;
         plane.vertices[index].z = noiseValue;
-        // console.log("noiseValue", noiseValue);
-        // X_OFFSET += 0.000001;
       }
+      Y_OFFSET += 0.00056;
     }
-    X_OFFSET += 1;
-
-    // genNoise(X_OFFSET);
   }
 
   animate() {
     requestAnimationFrame(this.animate.bind(this));
-    // setTimeout(this.animate.bind(this), 1000);
-    // console.log("nosie", noise);
     this.setZ();
 
     this.renderer.render(this.scene, this.camera);
